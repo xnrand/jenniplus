64398530f60347c558a33f8eb6b851eead8520a2
xnrand <xnrand@gmail.com>
2015-04-04 23:52:35 +0200

    infra done

---
+++ applyPatches.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# SHOULD be posix-compatible.
+
+log() {
+  printf '\n[$%s] %s\n' "$applypatcheslogsuffix" "$*"
+  "$@"
+}
+
+logcd() {
+  local dir="$1"
+  local applypatcheslogsuffix
+  shift
+  cd "$dir"
+  applypatcheslogsuffix=":$dir" log "$@"
+  cd "$PDIR"
+}
+
+unset applypatcheslogsuffix
+set -o errexit
+
+PDIR="$(dirname "$(readlink -fn "$0")")"
+
+printf '[=] stored in %s\n' "$PDIR"
+
+cd $PDIR
+
+log git submodule update --init
+
+if ! [ -d jenniplus ] ; then
+  log git clone jenni jenniplus
+elif ! [ -d jenniplus/.git ] ; then
+  printf '[?] "jenniplus" exists but is not a git repository'
+else
+  logcd jenniplus git checkout origin/master
+  logcd jenniplus git pull origin master
+fi
+
+log cd jenniplus
+
+for patch in ../patches/* ; do
+  log git am "$patch"
+done
+
+printf '[=] applied patches successfully\n'
